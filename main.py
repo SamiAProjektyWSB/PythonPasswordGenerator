c600644fe8108fda3cfcb54d902b65b1da544986
Sami <sami.projektyezn@gmail.com>
2023-10-02 14:14:46 +0200

    added inputs

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+
+print("Welcome to password generator! Answer the questions below to help us generate perfect password for you :)")
+pass_len = input("How many characters should password contain?(max number of letters): ")
+pass_min_len = input("Min length: ")
+pass_upper = input("Do the password need to have uppercase letters?(y/n): ")
+pass_digit = input("Do the password need to contain numbers?(y/n): ")
+pass_special_character = input("Do the password need to contain special characters?(y/n): ")
